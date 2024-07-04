bd8c565967d047d24cabc66f8ff32f9d2a8ceaff
Alejandro Botero <alejandro@albotero.com>
2024-07-03 21:49:44 -0500

    Filter categories

--- assets/js/populate-products.js
+++ assets/js/populate-products.js
@@ -5,25 +5,59 @@ const productosDiv = document.querySelector("main.productos")
 
 const populateListCategorias = () => {
   listCategoriasDiv.innerHTML = Object.entries(
-    PRODUCTS.reduce((acc, { category }) => ({ ...acc, [category]: acc[category] + 1 || 1 }), {})
+    PRODUCTS.reduce(
+      (acc, { category }) => ({
+        ...acc,
+        [category]: acc[category] + 1 || 1,
+        total: acc.total + 1,
+      }),
+      { total: 0 }
+    )
   )
-    .map(([name, count]) => `<div>${name} <span class="count">(${count})</span></div>`)
+    .map(([name, count]) => {
+      const category = name !== "total" && name
+      return `<button class="list-categorias-item" value="${category || ""}">
+          ${category || "Todos los productos"} <span class="count">(${count})</span>
+        </button>`
+    })
     .join("")
 }
 
-const populateProductos = () => {
-  productosDiv.innerHTML = PRODUCTS.map(
-    ({ id, title, price, img }) => `
+const populateProductos = (filter = "") => {
+  productosDiv.innerHTML = PRODUCTS.filter(({ category }) => !filter || category === filter)
+    .map(
+      ({ id, title, price, img }) => `
       <div class="product-card">
         <img src="./assets/img/${img}" alt="${title}" class="product-image" />
         <p class="product-title">${title}</p>
         <p class="product-price">COP$ ${price.toLocaleString()}</p>
-        <button class="product-buy" onclick="alert('COMPRADO:\\n${title}')" value="${id}">Comprar</button>
+        <button class="product-buy" value="${id}">Comprar</button>
       </div>`
-  ).join("")
+    )
+    .join("")
+  // Reset classes to remove selected class
+  document.querySelectorAll(".list-categorias-item").forEach((el) => {
+    el.className = "list-categorias-item"
+    // Set the new selected category
+    if (el.value === filter) el.classList.add("selected")
+  })
+}
+
+const buyProduct = (productId) => {
+  const { title, price } = PRODUCTS.find(({ id }) => id.toString() === productId)
+  alert(`COMPRADO:\n${title}\nPrecio: COP$ ${price.toLocaleString()}`)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   populateListCategorias()
   populateProductos()
+})
+
+document.addEventListener("click", (e) => {
+  // Filter categories
+  if (e.target.className === "list-categorias-item") return populateProductos(e.target.value)
+  // If user clicked on the count span, filter with parent value (button)
+  if (e.target.parentNode.className === "list-categorias-item") return populateProductos(e.target.parentNode.value)
+  // Buy button clicked
+  if (e.target.className === "product-buy") return buyProduct(e.target.value)
 })
